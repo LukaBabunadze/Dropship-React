0caa22240edf105e9ced16bc201e285d76225906
Luka Babunadze <>
2021-07-16 11:03:50 +0400

    Redux SORT SEARCH PRODUCTS

---
+++ src/Reducers/SearchReducer/SearchReducer.js
@@ -0,0 +1,17 @@
+// const initialState = {
+//     searchResult: [],
+// };
+//
+// const SearchReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case "PRODUCT_SEARCHED":
+//             return{
+//                 ...state,
+//                 searchResult: action.payload
+//             };
+//         default:
+//             return state;
+//     }
+// };
+//
+// export default SearchReducer;
